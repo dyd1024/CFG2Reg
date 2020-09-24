2626acfc172d4ad0fd1b53eb1f54317ca87c7794
dyd1024 <joker1024@yeah.net>
2020-09-24 22:35:15 +0800

    读取文件

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
+import util.PhraseFile;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("HelloWorld");
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        String filePath = args[0];
+        if(filePath != null){
+            File file = new File(filePath);
+            PhraseFile phraseFile = new PhraseFile(file);
+        }
+
     }
 }
--- src/util/PhraseFile.java
+++ src/util/PhraseFile.java
@@ -1,30 +1,37 @@
 package util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 
 public class PhraseFile {
 
     public Rule[] grammer;
 
     public  PhraseFile(File file){
-
-        Reader reader = null;
+        BufferedReader reader = null;
         try {
-            reader = new InputStreamReader(new FileInputStream(file));
-            int tempchar;
-            while ((tempchar = reader.read()) != -1) {
-                if (((char) tempchar) != '\r') {
-                    System.out.print((char) tempchar);
-                }
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                System.out.println("line " + line + ": " + tempString);
+                line++;
             }
             reader.close();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+
+                }
+            }
         }
+
 
 
     }
