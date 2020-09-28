668349c73bd6245ef0f3fae2da856e2e76489f9b
dyd1024 <joker1024@yeah.net>
2020-09-28 10:59:37 +0800

    打点

--- src/util/LRItem.java
+++ src/util/LRItem.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
-//inPut:ArrayList<Rule>，outPut:ArrayList<String> item
+//inPut:ArrayList<String>，outPut:ArrayList<String> item
 //non_Terminal::终结符集合
 //alphabet::字母表集合
 
@@ -15,44 +15,78 @@ public class LRItem {
 
     public LRItem(ArrayList<String> grammar){
         phrase(grammar);
-
+        dot(grammar);
+        System.out.println(item);
 
     }
 
 
-
+/*
+input::ArrayList<String> grammar
+result::
+non_Terminal::终结符集合
+alphabet::字母表集合
+ */
     public void phrase(ArrayList<String> grammar){
 
         Iterator i = grammar.iterator();                                                                                //得到解析输入文件的结果，Iterator遍历。
         int line = 0;
         while(i.hasNext()){
+            String s = grammar.get(line);
+            if(s.equals("")){
+                break;
+            }
+            non_Terminal.add(s.substring(0,1));                                                         //保存所有的非终结符
+            alphabet.add(s.substring(0,1));
 
-            non_Terminal.add(grammar.get(line).substring(0,1));                                                         //保存所有的非终结符
-            alphabet.add(grammar.get(line).substring(0,1));
-
-            if(grammar.get(line).substring(3).equals("epsilon")){
+            if(s.substring(3).equals("epsilon")){
                 alphabet.add(String.valueOf(0));
             }else {
-                char[] temp = grammar.get(line).toCharArray();
+                char[] temp = s.toCharArray();
                 for(int j=0;j<temp.length;j++){
                     if(temp[j] != '-' && temp[j] != '>'){
                         alphabet.add(String.valueOf(temp[j]));                                                          //得到字母表
                     }
                 }
             }
-//            System.out.println(grammar.get(line).leftSide+"->"+grammar.get(line).rightSide);
             i.next();
             line++;
         }
-
         System.out.println(non_Terminal);
         System.out.println(alphabet);
+    }
+
+
+
+    public void dot(ArrayList<String> grammar){
+
+        System.out.println(grammar);
+        item.add("S'->.S");                                                                                              //开始符号默认为S
+        item.add("S'->S.");
+        Iterator i = grammar.iterator();
+        int line = 0;
+        while(i.hasNext()){
+            String s = grammar.get(line);
+            if(s.equals("")){
+                break;
+            }
+            if(s.substring(3).equals("epsilon")){
+                item.add(s.substring(0,3)+".");
+                break;
+            }
+
+            for(int index=3; index < s.length()+1; index++){
+                StringBuffer stringBuffer = new StringBuffer(s);
+                item.add(stringBuffer.insert(index, '.').toString());
+            }
+            i.next();
+            line++;
+        }
     }
 }
 
 
 //非终结符的集合
 //从开始符号出发，得到S'->S
-//自动机用树结构表示（节点类，边上的字母如何表示）
-//打点的函数，每次调用后移一位   input：String  output：String
+
 //子集构造怎么搞  NFA确定化
--- src/util/PhraseFile.java
+++ src/util/PhraseFile.java
@@ -8,16 +8,14 @@ public class PhraseFile {
     //解析文件得到上下文无关文法的条目
     //grammar::解析文件得到的产生式
 
-
     public ArrayList<String> grammar = new ArrayList<>();
-
 
     public  PhraseFile(File file){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String tempString = null;                                                                               // 一次读入一行，直到读入null为文件结束
 
-            while ((tempString = reader.readLine()) != null) {                                                      //TODO没有判断文件输入是否合法，之后再补
+            while ((tempString = reader.readLine()) != null) {                                                     //TODO没有判断文件输入是否合法，之后再补
 
                 grammar.add(tempString);
             }
@@ -29,7 +27,3 @@ public class PhraseFile {
 
     }
 }
-
-//          str.indexOf(',')表示在整个字符串中检索，返回位置值
-//          StringBuffer stringBuffer = new StringBuffer(src);
-//          return stringBuffer.insert(position, dec).toString();                                                 //在字符串的某个位置插入新字符的方法
